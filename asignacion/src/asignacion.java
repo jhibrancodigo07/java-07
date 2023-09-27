9f32d4e47a630c870e0797be0e8783f9bbb1a485
edc31 <edc@test.com>
2023-09-27 14:49:08 -0600

    asignacion por terminar

---
+++ asignacion/src/asignacion.java
@@ -0,0 +1,10 @@
+public class asignacion {
+    public static void main(String[] args) throws Exception {
+        int a = 6;
+        a = 45;
+        System.out.println(" = | =a => " + a);
+        
+        
+    
+        }
+    }
