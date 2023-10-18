2d5aea5bbb8d52fc23bd3bab1e7904eb1d8e2975
erickcampos <erickcampos865@gmail.com>
2023-10-17 22:13:24 -0600

    scclose

--- ejerciciosjava/App.java
+++ ejerciciosjava/App.java
@@ -1,22 +1,23 @@
 package ejerciciosjava;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc=new Scanner(System.in);  // todo metodo debe estar en el main
+        Scanner sc = new Scanner(System.in); // todo metodo debe estar en el main
         String nombre;
         System.out.println("Escribe tu nombre");
-        nombre=sc.nextLine();
-        System.out.println("hola "+ nombre);
+        nombre = sc.nextLine();
+        System.out.println("hola " + nombre);
 
-        int a; // ingresar las otras varibales 
-        int b; 
+        int a; // ingresar las otras varibales
+        int b;
         int suma;
-        System.out.println("ingresa el primer numero");// utilizar una clase para imprimir 
-        a=Integer.parseInt(sc.nextLine());//utilizar un integrador con la varibale a definir 
+        System.out.println("ingresa el primer numero");// utilizar una clase para imprimir
+        a = Integer.parseInt(sc.nextLine());// utilizar un integrador con la varibale a definir
         System.out.println("a = " + a);
         System.out.println("ingresa el segundo numero");
-        b=Integer.parseInt(sc.nextLine());
+        b = Integer.parseInt(sc.nextLine());
         System.out.println("b =" + b);
         suma = a + b;// se realiza la suma de manera directa y despues se imprime
         System.out.println(suma);
@@ -25,5 +26,4 @@ public class App {
 
     }
 
-    
 }
