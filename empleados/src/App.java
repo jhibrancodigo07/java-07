2f18864824c2b0e153bd02938f82e07a9388ece6
erickcampos <erickcampos865@gmail.com>
2023-11-10 15:48:13 -0600

    errores

--- empleados/src/App.java
+++ empleados/src/App.java
@@ -1,24 +1,41 @@
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.List;
 
 import db.conexion.ConexionDB;
+import db.conexion.Repositorys.GeneroRepository;
+import db.conexion.models.Genero;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        try (Connection connection = ConexionDB.obtenerConexion()) {
-            System.out.println("base conectada");
+        Genero nuevoGenero = new Genero(2, "hayabusa");
+        List<Genero> generos = new ArrayList<>();
+        GeneroRepository generoRepository = new GeneroRepository();
 
-        } catch (Exception e) {
-            System.out.println("base no conectada");
-
-        }
+        generoRepository.agregar(nuevoGenero);
+        generos = generoRepository.recuperarTodos();
+        for (Genero genero : generos) {
+            System.out.println("nombre: " + genero.getNombre());
         }
     }
-//Connection conexion;
-       // try{
-            //Class.forName("com.mysql.cj.jdbc.driver");
-           // conexion = DriverManager.getConnection("jdbc:msql://localhost:3306/empleados", "root", "root");
-           // System.out.println("se conecto a la base de datos");
-       // } catch (Exception e){
-           // System.out.println("error al conectar");
+}
+// try (Connection connection = ConexionDB.obtenerConexion()) {
+// System.out.println("base conectada");
+
+// } catch (Exception e) {
+// System.out.println("base no conectada");
+
+// }
+// }
+// }
+// Connection conexion;
+// try{
+// Class.forName("com.mysql.cj.jdbc.driver");
+// conexion =
+// DriverManager.getConnection("jdbc:msql://localhost:3306/empleados", "root",
+// "root");
+// System.out.println("se conecto a la base de datos");
+// } catch (Exception e){
+// System.out.println("error al conectar");
--- empleados/src/db/conexion/Repositorys/GeneroRepository.java
+++ empleados/src/db/conexion/Repositorys/GeneroRepository.java
@@ -1,7 +1,13 @@
 package db.conexion.Repositorys;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
+import db.conexion.ConexionDB;
 import db.conexion.models.Genero;
 import interfaces.Repositoryinterface;
 
@@ -15,7 +21,19 @@ public class GeneroRepository implements Repositoryinterface<Genero> {
 
     @Override
     public void agregar(Genero entidad) {
-        // TODO Auto-generated method stub
+        try (Connection connection = ConexionDB.obtenerConexion()) {
+            String q = "INSERT INTO generos VALUES(null,?)";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(q);) {
+                preparedStatement.setString(1, entidad.getNombre());
+                preparedStatement.executeUpdate();
+            } catch (Exception e) {
+                System.out.println("error al agregar genero");
+            }
+
+        } catch (Exception e) {
+            System.out.println("error al conectar");
+
+        }
 
     }
 
@@ -33,8 +51,34 @@ public class GeneroRepository implements Repositoryinterface<Genero> {
 
     @Override
     public List<Genero> recuperarTodos() {
-        // TODO Auto-generated method stub
+        try (Connection connection = ConexionDB.obtenerConexion()) {
+            String q = "SELECT * FROM generos";
+            Statement statement = (Statement) connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(q);
+            while (resultSet.next()) {
+                System.out.println("id" + resultSet.getLong("id") + "\tnombre" + resultSet.getString("nombre"));
+
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
         return null;
+    }
+
+    private List<Genero> dameListaGeneros(ResultSet resultSet) {
+        List<Genero> generos = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String nombre = resultSet.getString("nombre");
+                generos.add(new Genero(id, nombre));
+            }
+            return generos;
+        } catch (Exception e) {
+            System.out.println("error al recuperar generos");
+        }
+        return generos;
     }
 
 }
