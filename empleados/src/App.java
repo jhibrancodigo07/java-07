788807e2ab918060e3153b0cd784917ccdd4ed9b
erickcampos <erickcampos865@gmail.com>
2023-11-14 15:57:10 -0600

    proyecto final

--- empleados/src/App.java
+++ empleados/src/App.java
@@ -1,27 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import Screens.VentanaLogin;
 import db.conexion.Repositorys.GenerosRepository;
 import db.conexion.models.Genero;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        GenerosRepository generosRepository = new GenerosRepository();
-        List<Genero> generos = new ArrayList<>();
-        GenerosRepository generoRepository = new GenerosRepository();
-        generos = generosRepository.recuperarTodos();
-        for (Genero genero : generos) {
-            System.out.println("nombre:" + genero.getNombre());
-        }
+        VentanaLogin vl = new VentanaLogin("Mi primera ventana");
+        vl.setDefaultCloseOperation(vl.EXIT_ON_CLOSE);
+        vl.setVisible(true);
 
-        Genero Generos = new Genero(null, "hayabusa");
-        List<Genero> generos1 = new ArrayList<>();
-        GenerosRepository generosRepository1 = new GenerosRepository();
-        generos = generosRepository.recuperarTodos();
-        generosRepository.agregar(Generos);
-        for (Genero genero : generos) {
-            System.out.println("id: " + genero.getId() + "\tnombre" + genero.getNombre());
-        }
+        /*
+         * GenerosRepository generosRepository = new GenerosRepository();
+         * List<Genero> generos = new ArrayList<>();
+         * GenerosRepository generoRepository = new GenerosRepository();
+         * generos = generosRepository.recuperarTodos();
+         * for (Genero genero : generos) {
+         * System.out.println("nombre:" + genero.getNombre());
+         * }
+         * 
+         * Genero Generos = new Genero(null, "hayabusa");
+         * List<Genero> generos1 = new ArrayList<>();
+         * GenerosRepository generosRepository1 = new GenerosRepository();
+         * generos = generosRepository.recuperarTodos();
+         * generosRepository.agregar(Generos);
+         * for (Genero genero : generos) {
+         * System.out.println("id: " + genero.getId() + "\tnombre" +
+         * genero.getNombre());
+         * }
+         */
 
     }
 }
--- empleados/src/Screens/VentanaEmpleado.java
+++ empleados/src/Screens/VentanaEmpleado.java
@@ -4,19 +4,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JColorChooser;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import java.awt.GridBagLayout;
+import javax.swing.SwingConstants;
 
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import com.toedter.calendar.JDateChooser;
+
+import db.conexion.Repositorys.EmpleadoRepository;
+import db.conexion.Repositorys.GenerosRepository;
 import db.conexion.models.Empleado;
 
 public class VentanaEmpleado extends JFrame implements ActionListener {
+
     private Empleado empleado;
-    private JLabel textLabel;
+    private JLabel textJLabel;
     private JLabel numeroEmpleadoJLabel;
     private JLabel nombreJLabel;
     private JLabel domicilioJLabel;
@@ -29,7 +35,7 @@ public class VentanaEmpleado extends JFrame implements ActionListener {
     private JTextField domicilioJTextField;
     private JTextField telefonoJTextField;
     private JTextField emailJTextField;
-    private JColorChooser fehcaJDateChooser;
+    private JDateChooser fechaJDateChooser;
     private JComboBox<String> generoJComboBox;
     private JButton buscarJButton;
     private JButton guardarJButton;
@@ -37,27 +43,78 @@ public class VentanaEmpleado extends JFrame implements ActionListener {
     private JButton eliminarJButton;
     private JButton limpiarJButton;
 
-    /*
-     * public VentanaEmpleado(String titulo) {
-     * super(titulo);
-     * setSize(300, 200);
-     * setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     * this.setLayout(new GridBagLayout());
-     * crearComponentes();
-     */
+    private void GenerosRepository() {
+        GenerosRepository generosRepository = new GenerosRepository();
+        EmpleadoRepository empleadoRepository = new EmpleadoRepository();
+    }
 
-    // private void crearComponentes() {
+    private VentanaEmpleado(String titulo) {
+        super(titulo);
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new GridBagLayout());
+        crearComponentes();
+    }
+
+    private void crearComponentes() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        gbc.gridwidth = 4;
+        gbc.gridheight = 1;
+
+        textJLabel = new JLabel("Empleados");
+        textJLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        this.add(textJLabel, gbc);
+
+    }
+
     /*
-     * GridBagConstraints gbc = new GridBagConstraints();
-     * gbc.fill = GridBagConstraints.BOTH;
-     * gbc.insets = new Insets(10, 10, 10, 10);
-     * gbc.weightx = 1.0;
-     * gbc.weighty = 1.0;
-     * gbc.gridx = 0;
-     * gbc.gridy = 0;
+     * private void recuperarEmpleado(){
+     * // todo
+     * }
      * 
-     * gbc.gridwidth = 4;
-     * gbc.gridheight = 1;
+     * private void guardarEmpleado(){
+     * //todo
+     * }
+     * private void modificarEmpleado(){
+     * //todo
+     * }
+     * 
+     * private void eliminarEmpleado(){
+     * //todo
+     * }
+     * private void limpiarCampos(){
+     * //todo
+     * }
+     * private Empleado dameEmpleado(){
+     * //todo
+     * }
+     * private void actualizaPantalla(){
+     * //todo
+     * }
+     * private void llenarConboBox(){
+     * //todo
+     * }
+     * private boolean validarVacio(){
+     * //todo
+     * }
+     * private void mensajeVacio(){
+     * //todo
+     * }
+     * private void mensajeSelecion(){
+     * //todo
+     * }
+     * /*private boolean mensajeConfirmacion(String mensaje,String accion){
+     * //todo
+     * }
+     * private void mensajeEmailError(){
+     * /*
      */
 
     @Override
